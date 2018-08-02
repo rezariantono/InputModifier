82c0c938b7957dcc60463b79d0475e2394de9a10
Reza Sukarno <rezariantono@gmail.com>
2018-08-02 13:22:27 +0700

    Added modifier lowercase

--- Config/Modifiers.php
+++ Config/Modifiers.php
@@ -4,6 +4,7 @@ return [
     'trim' => \Xolura\InputModifier\Modifiers\Trim::class,
     'capitalize' => Xolura\InputModifier\Modifiers\Capitalize::class,
     'uppercase' => Xolura\InputModifier\Modifiers\Uppercase::class,
+    'lowercase' => Xolura\InputModifier\Modifiers\Lowercase::class,
     'passthrough' => \Xolura\InputModifier\Modifiers\Passthrough::class,
     'removeSpaces' => \Xolura\InputModifier\Modifiers\RemoveSpaces::class,
     'phoneNumber' => \Xolura\InputModifier\Modifiers\PhoneNumber::class,
